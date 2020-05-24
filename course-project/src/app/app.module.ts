5d048bfcfef2917c38a105ebdbd9d99f15522941
Kim Te <te.kimberly@gmail.com>
2020-05-24 14:43:16 -0400

    add template form

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-//import { HttpModule } from '@angular/core';
+//import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -36,7 +36,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
-   // HttpModule,
+    //HttpModule,
     AppRoutingModule
   ],
   providers: [ShoppingListServce],
